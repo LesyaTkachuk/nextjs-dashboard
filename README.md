811b8e69e4fc93a80cd97d349e337e9cbf0eb6a4
LesyaTkachuk <lesyalesya9876@gmail.com>
2024-11-21 17:37:02 +0200

    #1 add first page and basic structure

---
+++ README.md
@@ -0,0 +1,3 @@
+## Next.js Dashboard App
+
+For more information, see the [course curriculum](https://nextjs.org/learn) on the Next.js Website.
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+const InvoicesPage = ({}) => {
+  return (
+    <div>
+      Invoices Page
+    </div>
+  )
+};
+
+export default InvoicesPage;
