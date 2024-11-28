434995928975ac7119c45416f2994529f7bd12e7
LesyaTkachuk <lesyalesya9876@gmail.com>
2024-11-28 11:36:02 +0200

    #4 fix build errors

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+// ppr experimental feature could be used with the latest Next.js canary version
+// npm install next@canary
 const nextConfig: NextConfig = {
   //  experimental: {
   //   ppr: "incremental",
